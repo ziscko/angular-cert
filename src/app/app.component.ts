1e18e851a50e8377114cfa15782a23e36f00645c
Paco Glz <ziscko.team@gmail.com>
2023-08-13 12:35:28 -0600

    Add initial example components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-cert';
+  name = 'Angular'
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import './polyfills'
 
-import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
+import { AppModule } from './app/app.module'
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .then((ref) => {
+    // Ensure Angular destroys itself on hot reloads.
+    if ((window as any)['ngRef']) {
+      ;(window as any)['ngRef']?.destroy()
+    }
+    ;(window as any)['ngRef'] = ref
+
+    // Otherwise, log the boot error
+  })
+  .catch((err) => console.error(err))
