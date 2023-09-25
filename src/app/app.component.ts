8e9c833c3a3b1bd972cbd66a81c4abd854c80f0e
Juan Francisco González <j.g.gonzalez@accenture.com>
2023-09-25 00:14:29 -0600

    Remove css template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core'
-
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   name = 'Angular'
